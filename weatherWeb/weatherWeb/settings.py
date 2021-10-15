4ba1b1118720178a7e0fe89e99a8d0e5e37c6710
lucholagrotteria <lucho.lagrott@gmail.com>
2021-10-15 17:13:32 -0300

    Second commit

--- weatherWeb/weatherWeb/settings.py
+++ weatherWeb/weatherWeb/settings.py
@@ -119,8 +119,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = ''
+
+
 STATIC_URL = '/static/'
+STATIC_ROOT = ''
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
